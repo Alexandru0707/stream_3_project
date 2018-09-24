4c2b18b6219f9f40cd65a0b7a11e4219ebd34444
Alexandru0707 <alex_boys30@yahoo.com>
2018-09-24 16:21:39 +0100

    trying to fix static files

--- settings/base.py
+++ settings/base.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'photo_website.urls'
