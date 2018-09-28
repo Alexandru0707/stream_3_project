67b20815a37bbd50086d56cb2a69f6dc377e3568
Alexandru0707 <alex_boys30@yahoo.com>
2018-09-28 15:35:41 +0100

    styling blog

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,7 @@ class Post(models.Model):
     # author is linked to a registered
     # user, via the User model in the auth app.
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
+
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
