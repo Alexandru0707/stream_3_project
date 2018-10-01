6549e7934e6836be44334064e97b04009f54bcc7
Alexandru0707 <alex_boys30@yahoo.com>
2018-10-01 22:57:01 +0100

    gallery page

--- static/js/main.js
+++ static/js/main.js
@@ -5,3 +5,19 @@ function fade_out() {
   $(".alert-danger").fadeOut().empty();
 }
 
+$(document).ready(function() {
+    $('[data-fancybox="gallery"]').fancybox({
+        animationEffect: "zoom-in-out",
+        transitionEffect: "zoom-in-out",
+        buttons: [
+            "fullScreen",
+            "zoom",
+            "slideShow",
+            "close",
+            "thumbs"
+        ],
+        protect: true
+    });
+});
+
+
