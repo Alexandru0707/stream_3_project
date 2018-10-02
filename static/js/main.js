0ec791faa32551a9a3e123f06b7c7ec0e01e4ae6
Alexandru0707 <alex_boys30@yahoo.com>
2018-10-02 14:33:37 +0100

    loader to gallery and go to top button

--- static/js/main.js
+++ static/js/main.js
@@ -20,4 +20,38 @@ $(document).ready(function() {
     });
 });
 
+// ===== Scroll to Top ====
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
+
+
+
+
+
+ $(window).on('load', function () {
+      $(".bg_gallery").fadeIn(3000).show();
+      $(".spinner-wrapper").hide();
+      $("footer").fadeIn(3000);
+      $(".navbar-default").fadeIn(3000);
+
+
+
+ });
+
+
+
+
+
+
 
