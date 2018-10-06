a73953db9f49691d6c0ca4c35100a1c229e763fc
Alexandru0707 <alex_boys30@yahoo.com>
2018-10-06 18:45:53 +0100

    spelling corect

--- settings/base.py
+++ settings/base.py
@@ -154,11 +154,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST', 'Optional default value')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PAS', 'Optional default value')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PAS')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = os.getenv('FROM_EMAIL', 'Optional default value')
+DEFAULT_FROM_EMAIL = os.getenv('FROM_EMAIL')
 ACCOUNT_EMAIL_SUBJECT_PREFIX = 'Contact email received from my website'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
